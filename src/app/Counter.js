ee3f9c469306da309fd2a7bdd2ef566f00561de7
vaghasiyaurvi9 <vaghasiyaurvi9@gmail.com>
2023-06-30 12:44:35 +0530

    update

--- src/app/Counter.js
+++ src/app/Counter.js
@@ -18,8 +18,11 @@ export const Counter = createSlice({
             state.value -= 1
         }
     },
+    incrementByAmount: (state, action) => {
+        state.value += action.payload
+      },
     
   },
 })
-export const { increment, decrement } = Counter.actions
+export const { increment, decrement ,incrementByAmount} = Counter.actions
 export default Counter.reducer
